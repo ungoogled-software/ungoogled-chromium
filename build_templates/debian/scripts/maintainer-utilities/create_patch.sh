4430526f28896f8f16da714bc5aa92a18f08b6ea
Eloston <eloston@mail.com>
2015-12-10 21:54:58 -0800

    Updated ungoogled-chromium patches and added them to patch_order Tweaked Iridium and Inox patches to make them apply correctly Removed unecessary patches Debian patches Tweaked domain_patcher.sh to only operate on files and also parse .mm files Tweaked create_patch.sh to make it work without a GUI

--- build_templates/debian/scripts/maintainer-utilities/create_patch.sh
+++ build_templates/debian/scripts/maintainer-utilities/create_patch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TEXT_EDITOR_CMD=my_leafpad
+TEXT_EDITOR_CMD=nano
 DIFF_CMD="git diff --no-index"
 FILE_PATH=$1
 
@@ -17,5 +17,5 @@ fi
 cp ${FILE_PATH} ${FILE_PATH}.orig
 ${TEXT_EDITOR_CMD} ${FILE_PATH}
 read -p "Press [Enter] to continue:"
-${DIFF_CMD} ${FILE_PATH}.orig ${FILE_PATH} | tail --lines=+3 | sed "s|${FILE_PATH}.orig|${FILE_PATH}|g" | ${TEXT_EDITOR_CMD}
+${DIFF_CMD} ${FILE_PATH}.orig ${FILE_PATH} | tail --lines=+3 | sed "s|${FILE_PATH}.orig|${FILE_PATH}|g" | ${TEXT_EDITOR_CMD} -
 mv ${FILE_PATH}.orig ${FILE_PATH}
