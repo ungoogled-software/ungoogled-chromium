3580a48676a557ca74ec60b5aaf4d4ab41d611e6
Eloston <Eloston@users.noreply.github.com>
2016-03-31 14:41:45 -0700

    Add new patches * Disable copresence * Disable domain_reliability * Disable gcm (Closes #7) * Show infobar for patched domains * Change infobar message Reformat domain_patcher.sh and source_cleaner.sh Update prevent-trace-url-requests.patch

--- domain_patcher.sh
+++ domain_patcher.sh
@@ -1,3 +1,43 @@
-find . -path ./debian -prune -o -type f \( -name "*.h" -o -name "*.hh" -o -name "*.hpp" -o -name "*.hxx" -o -name "*.cc" -o -name "*.cpp" -o -name "*.cxx" -o -name "*.c" -o -name "*.h" -o -name "*.json" -o -name "*.js" -o -name "*.html" -o -name "*.htm" -o -name "*.py*" -o -name "*.grd" -o -name "*.sql" -o -name "*.idl" -o -name "*.mk" -o -name "*.gyp*" -o -name "Makefile" -o -name "makefile" -o -name "*.txt" -o -name "*.xml" -o -name "*.mm" -o -name "*.jinja*" \) -print | xargs -L1 -I{} sed -i -r -e 's/google([A-Za-z\-]*)\.com/9oo91e\1\.qjz9zk/g' -e 's/gstatic([A-Za-z\-]*)\.com/95tat1c\1\.qjz9zk/g' -e 's/chrome([A-Za-z\-]*)\.com/ch40me\1\.qjz9zk/g' -e 's/chromium([A-Za-z\-]*)\.org/ch40m1um\1\.qjz9zk/g' -e 's/mozilla([A-Za-z\-]*)\.org/m0z111a\1\.qjz9zk/g' -e 's/facebook([A-Za-z\-]*)\.com/f8c3b00k\1\.qjz9zk/g' -e 's/appspot([A-Za-z\-]*)\.com/8pp2p8t\1\.qjz9zk/g' -e 's/youtube([A-Za-z\-]*)\.com/y0u1ub3\1\.qjz9zk/g' -e 's/ytimg([A-Za-z\-]*)\.com/yt1mg\1\.qjz9zk/g' -e 's/gmail([A-Za-z\-]*)\.com/9ma1l\1\.qjz9zk/g' -e 's/doubleclick([A-Za-z\-]*)\.net/60u613cl1c4\1\.qjz9zk/g' {}
+find . -path ./debian -prune \
+-o -type f \( -name "*.h" \
+    -o -name "*.hh" \
+    -o -name "*.hpp" \
+    -o -name "*.hxx" \
+    -o -name "*.cc" \
+    -o -name "*.cpp" \
+    -o -name "*.cxx" \
+    -o -name "*.c" \
+    -o -name "*.h" \
+    -o -name "*.json" \
+    -o -name "*.js" \
+    -o -name "*.html" \
+    -o -name "*.htm" \
+    -o -name "*.py*" \
+    -o -name "*.grd" \
+    -o -name "*.sql" \
+    -o -name "*.idl" \
+    -o -name "*.mk" \
+    -o -name "*.gyp*" \
+    -o -name "Makefile" \
+    -o -name "makefile" \
+    -o -name "*.txt" \
+    -o -name "*.xml" \
+    -o -name "*.mm" \
+    -o -name "*.jinja*" \) \
+-print | xargs -L1 -I{} sed -i -r \
+    -e 's/google([A-Za-z\-]*)\.com/9oo91e\1\.qjz9zk/g' \
+    -e 's/gstatic([A-Za-z\-]*)\.com/95tat1c\1\.qjz9zk/g' \
+    -e 's/chrome([A-Za-z\-]*)\.com/ch40me\1\.qjz9zk/g' \
+    -e 's/chromium([A-Za-z\-]*)\.org/ch40m1um\1\.qjz9zk/g' \
+    -e 's/mozilla([A-Za-z\-]*)\.org/m0z111a\1\.qjz9zk/g' \
+    -e 's/facebook([A-Za-z\-]*)\.com/f8c3b00k\1\.qjz9zk/g' \
+    -e 's/appspot([A-Za-z\-]*)\.com/8pp2p8t\1\.qjz9zk/g' \
+    -e 's/youtube([A-Za-z\-]*)\.com/y0u1ub3\1\.qjz9zk/g' \
+    -e 's/ytimg([A-Za-z\-]*)\.com/yt1mg\1\.qjz9zk/g' \
+    -e 's/gmail([A-Za-z\-]*)\.com/9ma1l\1\.qjz9zk/g' \
+    -e 's/doubleclick([A-Za-z\-]*)\.net/60u613cl1c4\1\.qjz9zk/g' \
+    -e 's/googlezip\.net/9oo91e21p\.qjz9zk/g' \
+    -e 's/beacon[1-9]\.gvt[1-9]\.com/b3ac0n\1\.9vt\2\.qjz9zk/g' \
+    -e 's/microsoft\.com/m1cr050ft\.qjz9zk/g' {}
 
 exit 0;
--- source_cleaner.sh
+++ source_cleaner.sh
@@ -1,6 +1,46 @@
 # A script that strips unwanted files
 
 # Delete all binary files
-find . -path ./debian -prune -o -path ./third_party/icu/source -prune -o -path ./third_party/liblouis/src/tables -prune -o -path ./components/dom_distiller/core/data/distillable_page_model.bin -prune -o -path ./components/dom_distiller/core/data/distillable_page_model_new.bin -prune -o -path ./third_party/skia/resources -prune -o -path ./third_party/deqp/src/data -prune -o -type f -not \( -empty \) -not \( -name "*.ttf" -o -name "*.png" -o -name "*.jpg" -o -name "*.webp" -o -name "*.gif" -o -name "*.ico" -o -name "*.mp3" -o -name "*.wav" -o -name "*.icns" -o -name "*.woff" -o -name "*.woff2" -o -name "*Makefile" -o -name "*makefile" -o -name "*.xcf" -o -name "*.cur" -o -name "*.pdf" -o -name "*.ai" -o -name "*.h" -o -name "*.c" -o -name "*.cpp" -o -name "*.cc" -o -name "*.mk" -o -name "*.bmp" -o -name "*.py" -o -name "*.xml" -o -name "*.html" -o -name "*.js" -o -name "*.json" -o -name "*.txt" -o -name "*.TXT" \) -not \( -exec grep -Iq . {} \; \) -print | xargs -L1 -I{} rm {}
+find . -path ./debian -prune \
+    -o -path ./third_party/icu/source -prune \
+    -o -path ./third_party/liblouis/src/tables -prune \
+    -o -path ./components/dom_distiller/core/data/distillable_page_model.bin -prune \
+    -o -path ./components/dom_distiller/core/data/distillable_page_model_new.bin -prune \
+    -o -path ./third_party/skia/resources -prune \
+    -o -path ./third_party/deqp/src/data -prune \
+    -o -type f -not \( -empty \) -not \( -name "*.ttf" \
+        -o -name "*.png" \
+        -o -name "*.jpg" \
+        -o -name "*.webp" \
+        -o -name "*.gif" \
+        -o -name "*.ico" \
+        -o -name "*.mp3" \
+        -o -name "*.wav" \
+        -o -name "*.icns" \
+        -o -name "*.woff" \
+        -o -name "*.woff2" \
+        -o -name "*Makefile" \
+        -o -name "*makefile" \
+        -o -name "*.xcf" \
+        -o -name "*.cur" \
+        -o -name "*.pdf" \
+        -o -name "*.ai" \
+        -o -name "*.h" \
+        -o -name "*.c" \
+        -o -name "*.cpp" \
+        -o -name "*.cc" \
+        -o -name "*.mk" \
+        -o -name "*.bmp" \
+        -o -name "*.py" \
+        -o -name "*.xml" \
+        -o -name "*.html" \
+        -o -name "*.js" \
+        -o -name "*.json" \
+        -o -name "*.txt" \
+        -o -name "*.TXT" \) \
+    -not \( -exec grep -Iq . {} \; \) -print | xargs -L1 -I{} rm {}
+
+# Delete domain_reliability files
+rm -r ./components/domain_reliability/baked_in_configs/*
 
 exit 0;
