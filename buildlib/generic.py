926e892e99ff2a31a32e54456a027ad2874fa9b7
Eloston <Eloston@users.noreply.github.com>
2016-07-21 21:43:32 -0700

    Reorganize resources into new directories Rename building module to buildlib Refactor buildlib for easier and cleaner platform extensions Update developer utilities to work with buildlib

--- buildlib/generic.py
+++ buildlib/generic.py
@@ -27,9 +27,24 @@ import re
 import subprocess
 import logging
 import configparser
+import distutils.dir_util
 
 class GenericPlatform:
-    def __init__(self, version, revision, logger=None, sandbox_root=pathlib.Path("build_sandbox")):
+    # Define default paths and file names. Some may be overridden by the methods
+    COMMON_RESOURCES = pathlib.Path("resources", "common")
+    PLATFORM_RESOURCES = None
+    CLEANING_LIST = pathlib.Path("cleaning_list")
+    DOMAIN_REGEX_LIST = pathlib.Path("domain_regex_list")
+    DOMAIN_SUBSTITUTION_LIST = pathlib.Path("domain_substitution_list")
+    PATCHES = pathlib.Path("patches")
+    PATCH_ORDER = pathlib.Path("patch_order")
+    GYP_FLAGS = pathlib.Path("gyp_flags")
+    GN_ARGS = pathlib.Path("gn_args.ini")
+    UNGOOGLED_DIR = pathlib.Path(".ungoogled")
+    SANDBOX_ROOT = pathlib.Path("build_sandbox")
+    BUILD_OUTPUT = pathlib.Path("out", "Release") # Change this for GN
+
+    def __init__(self, version, revision, logger=None, sandbox_root=None):
         self.version = version
         self.revision = revision
 
@@ -46,15 +61,18 @@ class GenericPlatform:
             logger.addHandler(console_handler)
         self.logger = logger
 
-        if sandbox_root.exists():
-            if not sandbox_root.is_dir():
+        if sandbox_root is None:
+            self.sandbox_root = self.SANDBOX_ROOT
+        else:
+            self.sandbox_root = sandbox_root
+        if self.sandbox_root.exists():
+            if not self.sandbox_root.is_dir():
                 raise Exception("sandbox_root exists, but is not a directory")
         else:
-            self.logger.info("sandbox_root does not exist. Creating...")
-            sandbox_root.mkdir()
-        self.sandbox_root = sandbox_root
+            self.logger.info("Sandbox root does not exist. Creating...")
+            self.sandbox_root.mkdir()
 
-        self.ungoogled_dir = self.sandbox_root / pathlib.Path(".ungoogled")
+        self.ungoogled_dir = self.sandbox_root / self.UNGOOGLED_DIR
         if self.ungoogled_dir.exists():
             if not self.ungoogled_dir.is_dir():
                 raise Exception("ungoogled_dir exists, but is not a directory")
@@ -67,6 +85,25 @@ class GenericPlatform:
         self.gn_command = None
         self.ninja_command = None
         self.build_output = None
+
+        self._ran_domain_substitution = False
+        self._domain_regex_cache = None
+
+    def _read_list_resource(self, file_name, is_binary=False):
+        if is_binary:
+            file_mode = "rb"
+        else:
+            file_mode = "r"
+        common_path = self.COMMON_RESOURCES / file_name
+        with common_path.open(file_mode) as f:
+            tmp_list = f.read().splitlines()
+        if not self.PLATFORM_RESOURCES is None:
+            platform_path = self.PLATFORM_RESOURCES / file_name
+            if platform_path.exists():
+                with platform_path.open(file_mode) as f:
+                    tmp_list.extend(f.read().splitlines())
+                    self.logger.debug("Successfully appended platform list")
+        return [x for x in tmp_list if len(x) > 0]
 
     def _check_source_archive(self):
         '''
@@ -126,16 +163,18 @@ class GenericPlatform:
                     self.logger.error("Exception thrown for tar member {}".format(tarinfo.name))
                     raise e
 
-    def _domain_substitute(self, regex_defs, file_list, log_warnings=True):
+    def _get_parsed_domain_regexes(self):
+        if self._domain_regex_cache is None:
+            self._domain_regex_cache = list()
+            for expression in self._read_list_resource(self.DOMAIN_REGEX_LIST, is_binary=True):
+                expression = expression.split(b'#')
+                self._domain_regex_cache.append((re.compile(expression[0]), expression[1]))
+        return self._domain_regex_cache
+
+    def _domain_substitute(self, regex_list, file_list, log_warnings=True):
         '''
-        Runs domain substitution with regex_strings over files file_list
+        Runs domain substitution with regex_list over files file_list
         '''
-        regex_list = list()
-        with regex_defs.open(mode="rb") as f:
-            for expression in f.read().splitlines():
-                if not expression == "":
-                    expression = expression.split(b'#')
-                    regex_list.append((re.compile(expression[0]), expression[1]))
         for path in file_list:
             try:
                 with path.open(mode="r+b") as f:
@@ -154,6 +193,27 @@ class GenericPlatform:
             except Exception as e:
                 self.logger.error("Exception thrown for path {}".format(path))
                 raise e
+
+    def _generate_patches(self, output_dir, run_domain_substitution):
+        platform_patch_order = self.PLATFORM_RESOURCES / self.PATCHES / self.PATCH_ORDER
+        platform_patches_exist = (not self.PLATFORM_RESOURCES is None) and platform_patch_order.exists()
+        with (self.COMMON_RESOURCES / self.PATCHES / self.PATCH_ORDER).open() as f:
+            new_patch_order = f.read()
+        if platform_patches_exist:
+            self.logger.debug("Using platform patches")
+            with platform_patch_order.open() as f:
+                new_patch_order += f.read()
+
+        distutils.dir_util.copy_tree(str(self.COMMON_RESOURCES / self.PATCHES), str(output_dir))
+        (output_dir / self.PATCH_ORDER).unlink()
+        distutils.dir_util.copy_tree(str(self.PLATFORM_RESOURCES / self.PATCHES), str(output_dir))
+        (output_dir / self.PATCH_ORDER).unlink()
+        with (output_dir / self.PATCH_ORDER).open("w") as f:
+            f.write(new_patch_order)
+
+        if run_domain_substitution:
+            self.logger.debug("Running domain substitution over patches...")
+            self._domain_substitute(self._get_parsed_domain_regexes(), self.sandbox_patches.rglob("*.patch"), log_warnings=False)
 
     def _gyp_generate_ninja(self, args_list, build_output, python2_command):
         command_list = list()
@@ -226,7 +286,7 @@ class GenericPlatform:
         self._run_ninja(build_output, ["gn"])
         return str(build_output / pathlib.Path("gn"))
 
-    def setup_chromium_source(self, check_if_exists=True, force_download=False, check_integrity=True, extract_archive=True, destination_dir=pathlib.Path("."), cleaning_list=pathlib.Path("cleaning_list"), archive_path=None, hashes_path=None):
+    def setup_chromium_source(self, check_if_exists=True, force_download=False, check_integrity=True, extract_archive=True, destination_dir=pathlib.Path("."), use_cleaning_list=True, archive_path=None, hashes_path=None):
         '''
         Sets up the Chromium source code in the build sandbox. It can download the source code in .tar.xz format, integrity check it, and extract it into the build sandbox while excluding any files in the cleaning list.
 
@@ -235,7 +295,7 @@ class GenericPlatform:
         If `check_integrity` is True, then the source archive will be integrity checked. Otherwise no hashes file will be downloaded and no integrity checking is done.
         If `extract_archive` is True, then the source archive will be extracted into the build sandbox.
         `destination_dir` specifies the directory for downloading the source archive and the hashes file to.
-        `cleaning_list` specifies the file containing the list of files to exclude from the source archive during extraction.
+        `use_cleaning_list` specifies whether to use the cleaning lists or not (common and platform)
         If `archive_path` is set, it must be a pathlib path instance that specifies the location to an existing source archive. It will cause the skipping of downloading the the source archive. It must be set alongside `hashes_path`.
         `hashes_path` must be a pathlib path that points to the hashes file. It will be ignored if `archive_path` is set to None.
         '''
@@ -277,24 +337,25 @@ class GenericPlatform:
 
         if extract_archive:
             self.logger.info("Extracting source archive into building sandbox...")
-            if cleaning_list is None:
-                self._extract_source_archive(list())
+            if use_cleaning_list:
+                list_obj = self._read_list_resource(self.CLEANING_LIST)
+                self._extract_source_archive(list_obj)
+                for i in list_obj:
+                    self.logger.warning("File does not exist in tar file: {}".format(i))
             else:
-                with cleaning_list.open() as f:
-                    self._extract_source_archive([x for x in f.read().splitlines() if x != ""])
+                self._extract_source_archive(list())
 
-    def setup_build_sandbox(self, run_domain_substitution=True, domain_regexes=pathlib.Path("domain_regex_list"), domain_sub_list=pathlib.Path("domain_substitution_list")):
+    def setup_build_sandbox(self, run_domain_substitution=True):
         '''
         Sets up the build sandbox. For now, this function can do domain substitution.
         '''
         if run_domain_substitution:
             self.logger.info("Running domain substitution over build sandbox...")
             def file_list_generator():
-                with domain_sub_list.open() as f:
-                    for x in f.read().splitlines():
-                        if x != "":
-                            yield self.sandbox_root / pathlib.Path(*pathlib.PurePosixPath(x).parts)
-            self._domain_substitute(domain_regexes, file_list_generator())
+                for x in self._read_list_resource(self.DOMAIN_SUBSTITUTION_LIST):
+                    yield self.sandbox_root / pathlib.Path(*pathlib.PurePosixPath(x).parts)
+            self._domain_substitute(self._get_parsed_domain_regexes(), file_list_generator())
+            self._ran_domain_substitution = True
 
     def apply_patches(self):
         # TODO: Use Python to apply patches defined in `patch_order`
@@ -329,17 +390,16 @@ class GenericPlatform:
     #    self._gn_write_args(config, build_output)
     #    self._gn_generate_ninja(build_output)
 
-    def generate_build_configuration(self, gyp_flags=pathlib.Path("gyp_flags"), build_output=pathlib.Path("out", "Release"), python2_command=None):
+    def generate_build_configuration(self, build_output=pathlib.Path("out", "Release"), python2_command=None):
         self.logger.info("Running gyp command...")
-        gyp_list = list()
-        with gyp_flags.open() as f:
-            gyp_list = f.read().splitlines()
-        self._gyp_generate_ninja(gyp_list, build_output, python2_command)
+        self._gyp_generate_ninja(self._read_list_resource(self.GYP_FLAGS), build_output, python2_command)
         self.build_output = build_output
 
-    def build(self):
+    def build(self, build_targets=["chrome"]):
         self.logger.info("Running build command...")
-        self._run_ninja(self.build_output, ["chrome"])
+        if self.build_output is None:
+            raise Exception("build_output member variable is not defined. Run generate_build_configuration() first or set it manually")
+        self._run_ninja(self.build_output, build_targets)
 
     def generate_package(self):
         # TODO: Create .tar.xz of binaries?
