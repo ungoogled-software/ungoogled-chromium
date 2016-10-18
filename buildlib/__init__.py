c2835dc3177bbb5c6d5a8db0dcd245f9b432d86c
Eloston <eloston@mail.com>
2016-10-18 01:33:42 -0700

    Add Arch Linux builder. Closes #68

--- buildlib/__init__.py
+++ buildlib/__init__.py
@@ -39,15 +39,19 @@ def get_builder(*args, **kwargs):
         from .macos import MacOSBuilder
         cls = MacOSBuilder
     elif sys.platform == "linux":
-        from .debian import DebianBuilder, DebianStretchBuilder, UbuntuXenialBuilder
         from ._external import distro
         dist_id, dist_version, dist_codename = distro.linux_distribution(
             full_distribution_name=False)
         if dist_id == "debian" and (dist_codename == "stretch" or
                                     dist_codename == "sid" or dist_version == "testing"):
+            from .debian import DebianStretchBuilder
             cls = DebianStretchBuilder
         elif dist_id == "ubuntu":
+            from .debian import UbuntuXenialBuilder
             cls = UbuntuXenialBuilder
+        elif dist_id == "arch":
+            from .archlinux import ArchLinuxBuilder
+            cls = ArchLinuxBuilder
         else:
             from .linux import LinuxStaticBuilder
             cls = LinuxStaticBuilder
