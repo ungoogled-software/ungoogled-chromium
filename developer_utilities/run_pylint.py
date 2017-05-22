0404ae83e785c7d40f2db6fa900101b93b1bc04f
Eloston <Eloston@users.noreply.github.com>
2017-05-22 11:12:41 -0700

    Switch license from GPLv3 to BSD-3-clause

--- developer_utilities/run_pylint.py
+++ developer_utilities/run_pylint.py
@@ -1,23 +1,8 @@
 #!/usr/bin/env python3
 
-# ungoogled-chromium: A Google Chromium variant for removing Google integration and
-# enhancing privacy, control, and transparency
-# Copyright (C) 2016  Eloston
-#
-# This file is part of ungoogled-chromium.
-#
-# ungoogled-chromium is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# ungoogled-chromium is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with ungoogled-chromium.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright (c) 2017 The ungoogled-chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 if __name__ == "__main__":
     from pylint import epylint as lint
--- utilikit/build_gn.py
+++ utilikit/build_gn.py
@@ -1,24 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-# ungoogled-chromium: Modifications to Google Chromium for removing Google integration
-# and enhancing privacy, control, and transparency
-# Copyright (C) 2017  Eloston
-#
-# This file is part of ungoogled-chromium.
-#
-# ungoogled-chromium is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# ungoogled-chromium is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with ungoogled-chromium.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright (c) 2017 The ungoogled-chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 """Builds GN"""
 
