3b39bca667518c2519c2f6fa2aab0d68fbe163a2
Eloston <eloston@programmer.net>
2018-02-16 21:30:23 +0000

    developer_utilities: Refactor and update to buildkit

--- developer_utilities/invert_domain_substitution.py
+++ developer_utilities/invert_domain_substitution.py
@@ -17,14 +17,23 @@ sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 from buildkit import domain_substitution
 from buildkit.common import get_logger
 from buildkit.config import ConfigBundle
+from buildkit.cli import NewBaseBundleAction
 sys.path.pop(0)
 
 def main(arg_list=None):
+    """CLI entrypoint"""
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument(
-        'bundle', type=lambda x: ConfigBundle(Path(x)),
-        help='The config bundle path to use.')
-
+    config_group = parser.add_mutually_exclusive_group()
+    config_group.add_argument(
+        '-b', '--base-bundle', metavar='NAME', dest='bundle',
+        action=NewBaseBundleAction,
+        help=('The base config bundle name to use (located in resources/config_bundles). '
+              'Mutually exclusive with --user-bundle-path. '))
+    config_group.add_argument(
+        '-u', '--user-bundle', metavar='PATH', dest='bundle',
+        type=lambda x: ConfigBundle(Path(x)),
+        help=('The path to a user bundle to use. '
+              'Mutually exclusive with --base-bundle-name. '))
     args = parser.parse_args(args=arg_list)
 
     try:
--- developer_utilities/set_quilt_vars.sh
+++ developer_utilities/set_quilt_vars.sh
@@ -1,14 +1,15 @@
 # Sets quilt variables for updating the patches
 # Make sure to run this with the shell command "source" in order to inherit the variables into the interactive environment
 
-# Assume the script is run from the repository
+# Assumes the script is run from the repository
 REPO_ROOT=$(dirname $(readlink -f $0))
 
 export QUILT_PATCHES="$REPO_ROOT/resources/patches"
-export QUILT_SERIES="$REPO_ROOT/build/updating_patch_order"
+export QUILT_SERIES="$REPO_ROOT/buildspace/updating_patch_order.list"
+# Options below borrowed from Debian
 export QUILT_PATCH_OPTS="--reject-format=unified"
-#export QUILT_DIFF_ARGS="-p ab --no-timestamps --no-index --color=auto"
-#export QUILT_REFRESH_ARGS="-p ab --no-timestamps --no-index"
+export QUILT_DIFF_ARGS="-p ab --no-timestamps --no-index --color=auto"
+export QUILT_REFRESH_ARGS="-p ab --no-timestamps --no-index"
 export QUILT_COLORS="diff_hdr=1;32:diff_add=1;34:diff_rem=1;31:diff_hunk=1;33:diff_ctx=35:diff_cctx=33"
-export QUILT_NO_DIFF_TIMESTAMPS=1
-export QUILT_NO_DIFF_INDEX=1
+#export QUILT_NO_DIFF_TIMESTAMPS=1
+#export QUILT_NO_DIFF_INDEX=1
