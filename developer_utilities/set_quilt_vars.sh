0f5b292c59a5a0f39bb57476f0a3dd49b1d17eca
Eloston <eloston@programmer.net>
2018-04-13 18:17:57 +0000

    developer_utilities: Fix set_quilt_vars.sh quilt settings

--- developer_utilities/set_quilt_vars.sh
+++ developer_utilities/set_quilt_vars.sh
@@ -4,17 +4,30 @@
 # There is some problem with the absolute paths in QUILT_PATCHES and QUILT_SERIES breaking quilt
 # (refresh and diff don't read QUILT_*_ARGS, and series displays absolute paths instead of relative)
 # Specifying a quiltrc file fixes this, so "--quiltrc -" fixes this too.
+# One side effect of '--quiltrc -' is that we lose default settings from /etc/quilt.quiltrc, so they are redefined below.
 alias quilt='quilt --quiltrc -'
 
-# Assumes the script is run from the repository
-REPO_ROOT=$(dirname $(readlink -f $0))
+# Assume this script lives within the repository
+REPO_ROOT=$(dirname $(dirname $(readlink -f ${BASH_SOURCE[0]})))
 
 export QUILT_PATCHES="$REPO_ROOT/resources/patches"
-export QUILT_SERIES="$REPO_ROOT/buildspace/updating_patch_order.list"
-# Options below borrowed from Debian
-export QUILT_PATCH_OPTS="--reject-format=unified"
+export QUILT_SERIES=$(readlink -f "$REPO_ROOT/buildspace/updating_patch_order.list")
+
+if [ -z "$QUILT_SERIES" ]; then
+    printf '%s\n' 'ERROR: QUILT_SERIES file not found.' >&2
+    return
+fi
+
+# Options below borrowed from Debian and default quilt options (from /etc/quilt.quiltrc on Debian)
+export QUILT_PUSH_ARGS="--color=auto"
+export QUILT_DIFF_OPTS="--show-c-function"
+export QUILT_PATCH_OPTS="--unified --reject-format=unified"
 export QUILT_DIFF_ARGS="-p ab --no-timestamps --no-index --color=auto"
 export QUILT_REFRESH_ARGS="-p ab --no-timestamps --no-index"
 export QUILT_COLORS="diff_hdr=1;32:diff_add=1;34:diff_rem=1;31:diff_hunk=1;33:diff_ctx=35:diff_cctx=33"
-#export QUILT_NO_DIFF_TIMESTAMPS=1
-#export QUILT_NO_DIFF_INDEX=1
+export QUILT_SERIES_ARGS="--color=auto"
+export QUILT_PATCHES_ARGS="--color=auto"
+
+# When non-default less options are used, add the -R option so that less outputs
+# ANSI color escape codes "raw".
+[ -n "$LESS" -a -z "${QUILT_PAGER+x}" ] && export QUILT_PAGER="less -FRX"
