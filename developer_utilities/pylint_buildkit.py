31f74b1d8a88fba98523f32ef6999c9d12ded60c
Eloston <eloston@programmer.net>
2018-06-04 05:17:07 +0000

    Extend pylint_buildkit.py and add to .travis.yml

--- developer_utilities/pylint_buildkit.py
+++ developer_utilities/pylint_buildkit.py
@@ -4,11 +4,36 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import argparse
+
 if __name__ == '__main__':
     from pylint import epylint as lint
     import pathlib
 
-    lint.lint(filename=str(pathlib.Path(__file__).parent.parent / 'buildkit'), options=[
-        '--disable=locally-disabled',
+    parser = argparse.ArgumentParser(description='Run Pylint over buildkit')
+    parser.add_argument(
+        '--hide-fixme', action='store_true',
+        help='Hide "fixme" Pylint warnings.')
+    parser.add_argument(
+        '--show-locally-disabled', action='store_true',
+        help='Show "locally-disabled" Pylint warnings.')
+    args = parser.parse_args()
+
+    disable = list()
+
+    if args.hide_fixme:
+        disable.append('fixme')
+    if not args.show_locally_disabled:
+        disable.append('locally-disabled')
+
+    result = lint.lint(filename=str(pathlib.Path(__file__).parent.parent / 'buildkit'), options=[
+        '--disable={}'.format(','.join(disable)),
         '--jobs=4',
         '--ignore=third_party'])
+
+    if result != 0:
+        print('WARNING: {}() returned non-zero result: {}'.format(
+            '.'.join((lint.lint.__module__, lint.lint.__name__)), result))
+        exit(1)
+
+    exit(0)
