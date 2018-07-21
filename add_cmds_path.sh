2bcee6553dbafc8c9a5b5ffa4770a8bfc48a0fd0
Eloston <eloston@programmer.net>
2018-07-21 19:28:53 +0000

    buildkit: Remove cmds and add_cms_path.sh
    buildkit shouldn't normally be invoked by the end-user, so this
    convenience is unnecessary. Users who really need to use the CLI can
    just use "python3 -m buildkit" or something to that effect.

--- add_cmds_path.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Set PATH variable to cmds/ directory on UNIX shell-compatible systems
-# Run this script with "source" in order for the PATH variable to take effect
-export PATH=$(dirname $(readlink -f $0))/cmds:$PATH
