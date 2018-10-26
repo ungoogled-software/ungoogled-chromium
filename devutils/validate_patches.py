a247b6537b06a857d02f0e6199225a69a4fb741f
Eloston <eloston@programmer.net>
2018-10-26 02:03:37 +0000

    devutils/validate_patches.py: Increase HTTP request backoff factor to 2

--- devutils/validate_patches.py
+++ devutils/validate_patches.py
@@ -62,7 +62,7 @@ try:
                 total=10,
                 read=10,
                 connect=10,
-                backoff_factor=1,
+                backoff_factor=2,
                 status_forcelist=urllib3.Retry.RETRY_AFTER_STATUS_CODES,
                 raise_on_status=False))
         session.mount('http://', http_adapter)
