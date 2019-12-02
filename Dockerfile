fb940f2d25bf7a768bc634461eb066bc86703e52
Albert Tang <tangalbert919@yahoo.com>
2019-12-02 13:12:40 -0600

    Fix the "Not Found" problem

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM alpine
 
 COPY devutils/create_new_tag.sh /create_new_tag.sh
 
-RUN apk update && apk add git curl bash
+RUN apk update && apk add git curl bash jq
 
 ENTRYPOINT ["/create_new_tag.sh"]
--- devutils/create_new_tag.sh
+++ devutils/create_new_tag.sh
@@ -11,7 +11,8 @@ UNGOOGLED_REVISION=$(cat revision.txt)
 UPDATED_TAG="${CHROMIUM_VERSION}-${UNGOOGLED_REVISION}"
 
 # Post a new tag to repo through GitHub API
-curl -s -X POST https://api.github.com/repos/$REPO_OWNER/ungoogled-chromium/git/refs \
+git_refs_url=$(jq .repository.git_refs_url $GITHUB_EVENT_PATH | tr -d '"' | sed 's/{\/sha}//g')
+curl -s -X POST $git_refs_url \
 -H "Authorization: token $GITHUB_TOKEN" \
 -d @- << EOF
 
