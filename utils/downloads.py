cb89aa5a5e22e8824edc6c35ed534003f979d97e
shubhendra <withshubh@gmail.com>
2021-03-21 02:30:39 +0530

    Use literal syntax instead of function calls to create data structure
    Signed-off-by: shubhendra <withshubh@gmail.com>

--- utils/downloads.py
+++ utils/downloads.py
@@ -88,7 +88,7 @@ class DownloadInfo: #pylint: disable=too-few-public-methods
             if name in self._passthrough_properties:
                 return self._section_dict.get(name, fallback=None)
             if name == 'hashes':
-                hashes_dict = dict()
+                hashes_dict = {}
                 for hash_name in (*self._hashes, 'hash_url'):
                     value = self._section_dict.get(hash_name, fallback=None)
                     if value:
